22d1cedcef2f2648f2a12c024f47dd311b7ae092
Trevor Tsai <trevortsai1@gmail.com>
2023-08-21 00:37:20 -0700

    Updated main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import dotenv
 # Globals
 logging.basicConfig()
 log: logging.Logger = logging.getLogger()
-cl: instagrapi.Client = instagrapi.Client(logger=log)
 DEFAULT_SESSION_FILEPATH: str = "session.json"
+
 
 def get_session_filepath():
     res = os.getenv("SESSION_FILEPATH")
@@ -19,7 +19,8 @@ def get_session_filepath():
     else:
         return res
 
-def login_user():
+
+def login_user(client: instagrapi.Client):
     """
     Attempts to login to Instagram first with session data, then with credentials if failed
     """
@@ -31,7 +32,7 @@ def login_user():
 
     session = False
     if os.path.exists(SESSION_FILEPATH):
-        session = cl.load_settings(SESSION_FILEPATH)
+        session = client.load_settings(SESSION_FILEPATH)
 
     # Load from environment variables
     ACCOUNT_USERNAME = os.getenv("ACCOUNT_USERNAME")
@@ -39,22 +40,22 @@ def login_user():
 
     if session:
         try:
-            cl.set_settings(session)
-            cl.login(ACCOUNT_USERNAME, ACCOUNT_PASSWORD)
+            client.set_settings(session)
+            client.login(ACCOUNT_USERNAME, ACCOUNT_PASSWORD)
 
             # check if session is valid
             try:
-                cl.get_timeline_feed()
+                client.get_timeline_feed()
             except instagrapi.LoginRequired:
                 log.info("Session is invalid, need to login via username and password")
 
-                old_session = cl.get_settings()
+                old_session = client.get_settings()
 
                 # use the same device uuids across logins
-                cl.set_settings({})
-                cl.set_uuids(old_session["uuids"])
+                client.set_settings({})
+                client.set_uuids(old_session["uuids"])
 
-                cl.login(ACCOUNT_USERNAME, ACCOUNT_PASSWORD)
+                client.login(ACCOUNT_USERNAME, ACCOUNT_PASSWORD)
             login_via_session = True
         except Exception as e:
             log.info("Couldn't login user using session information: %s" % e)
@@ -62,28 +63,40 @@ def login_user():
     if not login_via_session:
         try:
             log.info("Attempting to login via username and password. username: %s" % ACCOUNT_USERNAME)
-            if cl.login(ACCOUNT_USERNAME, ACCOUNT_PASSWORD):
+            if client.login(ACCOUNT_USERNAME, ACCOUNT_PASSWORD):
                 login_via_pw = True
-                cl.dump_settings(SESSION_FILEPATH)
+                client.dump_settings(SESSION_FILEPATH)
         except Exception as e:
             log.info("Couldn't login user using username and password: %s" % e)
 
     if not login_via_pw and not login_via_session:
         raise Exception("Couldn't login user with either password or session")
 
-def main():
-    log.info("Attempting login...")
-    login_user()
 
+def init() -> instagrapi.Client:
+    """
+    Initialize any pre-connection variables, loggers, etc. and returns a Client object
+    :return: Client object
+    """
 
-def init():
-    # Configure logger settings here
+    # Configure root logger settings here
     log.setLevel(logging.INFO)
+
+    # Client logger setup
+    client_logger = logging.getLogger("client")
+    client_logger.setLevel(logging.INFO)
 
     log.info("Reading environment variables...")
     dotenv.load_dotenv()
 
+    return instagrapi.Client(logger=client_logger)
+
+
+def main():
+    client = init()
+
+    log.info("Attempting login...")
+    login_user(client)
 
 if __name__ == '__main__':
-    init()
     main()
